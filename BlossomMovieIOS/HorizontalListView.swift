0924d342b51e17ff52ba3c262b38a6a7a44e6af0
troyfitz <troyfitzgerald3@gmail.com>
2026-01-01 01:50:08 -0500

    home view changes + horizontal list view + some new constants

---
+++ BlossomMovieIOS/HorizontalListView.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalListView.swift
+//  BlossomMovieIOS
+//
+//  Created by Troy Fitzgerald on 1/1/26.
+//
+
+import SwiftUI
+
+struct HorizontalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalListView()
+}
